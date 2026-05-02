ea22019dc5d54ecf1439a616e826d0950e2c4b95
Yoobin Kim <yoobinkim06@gmail.com>
2026-05-02 18:48:10 +0900

    Use secure-browser aware Google popup sign-in

--- firebase-init.js
+++ firebase-init.js
@@ -6,14 +6,12 @@
 
     if (firebase.apps && firebase.apps.length > 0) return;
 
-    const deployedHost = 'practice-todo-list-32af6.web.app';
     const defaultAuthDomain = 'practice-todo-list-32af6.firebaseapp.com';
-    const authDomain = window.location.hostname === deployedHost ? deployedHost : defaultAuthDomain;
 
     firebase.initializeApp({
         apiKey: 'AIzaSyD7rnA85vWTcyL-XF0OxacY83u9iQL7PTE',
         appId: '1:297469029810:web:1d8d1fbd5d20b2346248ab',
-        authDomain,
+        authDomain: defaultAuthDomain,
         databaseURL: '',
         measurementId: 'G-8WP9B9H3FX',
         messagingSenderId: '297469029810',
